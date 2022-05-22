9f158eeb5d773e180e5207a5a9c37c8cf206cb91
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2022-05-22 16:45:14 +0300

    initial commit

---
+++ src/main/java/com/github/kalininaleksandrv/simpletracker/SimpletrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.kalininaleksandrv.simpletracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpletrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpletrackerApplication.class, args);
+	}
+
+}
